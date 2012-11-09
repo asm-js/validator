23a8b2eb8000b0c09e5334b57425b971a288cc88
Dave Herman <dherman@mozilla.com>
2012-11-08 16:58:26 -0800

    ripped out all unification; updated for latest version of the system

--- test.js
+++ test.js
@@ -1,41 +1,84 @@
 var validate = require('./lib/validate');
 var types = require('./lib/types');
+var dict = require('dict');
 
-var header = [
-    "var floor = env.floor, sin = env.sin;",
-    "var H32 = env.Int32Array(buffer), HU32 = env.Uint32Array(buffer);"
-].join("\n");
+function explode(test) {
+    var __ALL__ = ["var imul = env.imul, sin = env.sin;",
+                   "var H32 = new env.Int32Array(buffer), HU32 = new env.Uint32Array(buffer);"];
 
-var f = function f(x, y, z, i, u) {
-    x = x|0;
-    y = y>>>0;
-    z = +z;
-    i = [i[0]>>>0, i[1]|0];
-    u = [u[0]>>>0, u[1]>>>0];
-}.toString();
+    return String(test).replace("__ALL__", __ALL__.join("\n    "));
+}
 
-var empty = function empty() { }.toString();
+function makeTest(f, msg, expect) {
+    f = explode(f);
+    var hasOwn = {}.hasOwnProperty;
 
-var test1 = [header, f, empty, "return empty;"].join("\n");
-
-function paramTypes(test, actual, expected, message) {
-    for (var i = 0, n = expected.length; i < n; i++) {
-        test.ok(actual instanceof types.arrow &&
-                actual.params.length === n &&
-                actual.params[i].equals(expected[i]),
-                message);
+    if (expect.pass) {
+        return function(test) {
+            try {
+                var report = validate(f);
+                var types = expect.types, exports = expect.exports;
+                if (types) {
+                    for (var key in types) {
+                        if (!hasOwn.call(types, key))
+                            continue;
+                        test.ok(report.has(key), msg + ": found function " + key);
+                        test.ok(report.get(key).type.equals(types[key]), msg + ": function " + key + " has type " + types[key]);
+                    }
+                }
+                if (exports) {
+                    for (var key in exports) {
+                        if (!hasOwn.call(exports, key))
+                            continue;
+                        test.ok(report.has(key), msg + ": found function " + key);
+                        var fn = report.get(key);
+                        test.deepEqual(fn && fn.exportedAs,
+                                       exports[key],
+                                       msg + ": function " + key + " exported as " + exports[key].join(", "));
+                    }
+                }
+            } catch (e) {
+                test.ok(false, msg + ": validation failed: " + e);
+            } finally {
+                test.done();
+            }
+        }
+    } else {
+        return function(test) {
+            test.throws(function() { validate(f); },
+                        function(e) {
+                            return e instanceof fail.ValidationError;
+                        },
+                        msg + ": should fail to validate");
+            test.done();
+        }
     }
 }
 
-exports.test1 = function(test) {
-    var report = validate(["buffer", "env"], test1);
+function test1(env, buffer) {
+    "use asm";
 
-    test.ok(report.has("f"));
-    test.ok(report.get("f").type.equals(types.arrow([types.int32, types.uint32, types.float64, types.int64, types.uint64], types.void)));
-    test.deepEqual(report.get("f").exportedAs, []);
-    test.ok(report.has("empty"));
-    test.ok(report.get("empty").type.equals(types.arrow([], types.void)));
-    test.deepEqual(report.get("empty").exportedAs, [null]);
+    __ALL__
 
-    test.done();
-};
+    function f(w, x, y, z) {
+        w = ~~w;
+        x = x|0;
+        y = y>>>0;
+        z = +z;
+    }
+    function empty() { }
+
+    return empty;
+}
+
+exports.test1 = makeTest(test1, "different parameter types", {
+    pass: true,
+    types: {
+        f: types.arrow([types.int, types.signed, types.unsigned, types.double], types.void),
+        empty: types.arrow([], types.void)
+    },
+    exports: {
+        f: [],
+        empty: [null]
+    }
+});
